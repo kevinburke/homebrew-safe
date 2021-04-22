9203e37179fc5e4cb4f8cd9c1228d712afe4aeb0
Kevin Burke <kevin@burke.dev>
2021-04-22 14:16:12 -0700

    Formula: fix crustls at version 0.4.0

--- Formula/crustls.rb
+++ Formula/crustls.rb
@@ -1,6 +1,8 @@
 class Crustls < Formula
   desc "C bindings for rustls"
   homepage "https://github.com/abetterinternet/crustls"
+  url "https://github.com/abetterinternet/crustls/archive/v0.4.0.tar.gz"
+  sha256 "6d30b06096750c52c718d70d0bc8259d6e3716aecc267d5bb0b8635e314f87e8"
   head "https://github.com/abetterinternet/crustls.git", :branch => "main"
 
   depends_on "rust" => :build
