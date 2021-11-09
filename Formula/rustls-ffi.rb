f4764a69f25253118a8ecab523a9b58436d2c0ab
Kevin Burke <kevin@burke.dev>
2021-11-08 21:00:18 -0800

    Formula: update rustls-ffi to v0.8.0

--- Formula/rustls-ffi.rb
+++ Formula/rustls-ffi.rb
@@ -1,8 +1,8 @@
 class RustlsFfi < Formula
   desc "C bindings for rustls"
   homepage "https://github.com/rustls/rustls-ffi"
-  url "https://github.com/rustls/rustls-ffi/archive/v0.7.2.tar.gz"
-  sha256 "68ad936fdd2a71416e7bea5d16fd400af806d6c01c0d3c0a6ff75b197eb5c883"
+  url "https://github.com/rustls/rustls-ffi/archive/v0.8.0.tar.gz"
+  sha256 "438159d436c63f0de1b9d830a74d92997e9fb545403e4ea8742a371cb2dfafbd"
   head "https://github.com/rustls/rustls-ffi.git", :branch => "main"
 
   depends_on "rust" => :build
