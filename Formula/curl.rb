2279adf72aac802a5a6f77b61a9189eb48c9247c
Kevin Burke <kevin@burke.dev>
2022-05-14 21:41:24 -0700

    Formula: revert curl to v7.82.0
    Curl v7.83.1 depends on Hyper v0.14.18.

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,11 +1,11 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
-  url "https://curl.se/download/curl-7.83.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.83.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-7.83.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-7.83.0.tar.bz2"
-  sha256 "247c7ec7521c4258e65634e529270d214fe32969971cccb72845e7aa46831f96"
+  url "https://curl.se/download/curl-7.82.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.82.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-7.82.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-7.82.0.tar.bz2"
+  sha256 "46d9a0400a33408fd992770b04a44a7434b3036f2e8089ac28b57573d59d371f"
   license "curl"
 
   livecheck do
@@ -43,23 +43,23 @@ class Curl < Formula
 
     # https://github.com/abetterinternet/crustls/wiki/Building-curl-with-crustls-and-Hyper
     args = %W[
+      --enable-debug
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
+      --without-ca-bundle
       --without-ca-path
-      --with-gssapi
       --with-libidn2
       --with-librtmp
       --without-libpsl
       --without-nghttp2
       --without-metalink
       --without-ssl
-      --enable-debug
+      --without-secure-transport
       --with-hyper=#{Formula["kevinburke/safe/hyper"].opt_prefix}
       --with-rustls=#{Formula["kevinburke/safe/rustls-ffi"].opt_prefix}
       --with-default-ssl-backend=rustls
     ]
-    # --without-ca-bundle
 
     cppflags = "-I#{Formula["kevinburke/safe/hyper"].opt_prefix}/include"
     on_macos do
