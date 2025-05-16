2c935c128795049cba3e6105f9b3bca312b8006a
Kevin Burke <kevin@burke.dev>
2025-05-15 22:28:04 -0700

    buildkite 0.18

--- Formula/buildkite.rb
+++ Formula/buildkite.rb
@@ -1,8 +1,8 @@
 class Buildkite < Formula
   desc "Buildkite CLI tool"
   homepage "https://github.com/kevinburke/buildkite"
-  url "https://github.com/kevinburke/buildkite/archive/v0.17.tar.gz"
-  sha256 "31f169561f737ba49c33d9d6846b8338c5959f904be02e38a1b3eb10bfb82a4f"
+  url "https://github.com/kevinburke/buildkite/archive/v0.18.tar.gz"
+  sha256 "c83e352a129a66815f33aa9a6a25fe1f5f7ed5055343538974c1a7e63fb6c620"
   license "MIT"
   head "https://github.com/kevinburke/buildkite.git", branch: "main"
 
